ce6f9dad881c4a590f5b6435f51b4d97f7e86004
TylerTeifke <tylerteifke@gmail.com>
2026-04-07 11:12:58 -0700

    Added the ability to search for all employees and products.

--- src/pages/search_menus/Search_All.js
+++ src/pages/search_menus/Search_All.js
@@ -8,13 +8,37 @@ const SearchAll = () => {
     //Will be used to navigate to other pages
     let navigate = useNavigate();
 
-    //Will be used to track the text the user wants displayed
+    //Will be used to display information on customers and employees
     const [text, setText] = useState([])
+    //Will be used to display information on products
+    const [products, setProducts] = useState([])
+    //Will be used to change which type of information to display
+    const [displayPeople, setDisplayPeople] = useState(true)
 
+    //will search for people
     async function handleFind(route) {
         try {
             const response = await axios.get(route);
             setText(response.data)
+            setDisplayPeople(true)
+        } 
+        catch (err) {
+            if (!err?.response) {
+                console.log("No server response")
+            } 
+
+            else {
+                console.log('Request Failed')
+            }
+        }
+    }
+
+    //will search for products
+    async function handleFindProducts() {
+        try {
+            const response = await axios.get('/products');
+            setProducts(response.data)
+            setDisplayPeople(false)
         } 
         catch (err) {
             if (!err?.response) {
@@ -32,23 +56,35 @@ const SearchAll = () => {
             <header className="header">
                 Click on one of the buttons below to see every entry
             </header>
-            <button onClick={() => setText([{id: "1", firstname: "John", lastname: "Smith"}])}>Employees</button>
+            <button onClick={() => handleFind('/employees')}>Employees</button>
             <br/>
             <br/>
             <button onClick={() => handleFind('/customers')}>Customers</button>
             <br/>
             <br/>
-            <button onClick={() => setText([{id: "1", firstname: "John", lastname: "Smith"}])}>Products</button>
+            <button onClick={() => handleFindProducts()}>Products</button>
             <br/>
             <br/>
             <button onClick={() => navigate("/Search")}>Back</button>
             <br/>
             <br/>
-            <p>
-                {text.map(customer => {
-                    return <li key={customer._id}>{customer.firstname} {customer.lastname}</li>
-                })}
-            </p>
+            {displayPeople && (
+                <p>
+                    {text.map(customer => {
+                        return <li key={customer._id}>{customer.firstname} {customer.lastname}</li>
+                    })}
+                </p>
+            )}
+            {!displayPeople && (
+                <p>
+                    {products.map(product => {
+                        return <li key={product._id}>Name: {product.details.name}, 
+                        Price: {product.details.price}, 
+                        Expiration Date: {product.expiration_date}
+                        </li>
+                    })}
+                </p>
+            )}
         </div>
     )
 }
